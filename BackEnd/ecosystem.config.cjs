4a1571c82d8d53d692d03b1c3739b31b5de14447
QTienTLS <55350592+QTienTLS@users.noreply.github.com>
2024-12-05 10:35:08 +0700

    Update ecosystem.config.cjs

--- BackEnd/ecosystem.config.cjs
+++ BackEnd/ecosystem.config.cjs
@@ -2,12 +2,15 @@ module.exports = {
   apps: [
   {
     name: 'MindStore-API',
-    script: 'bun',          // Đường dẫn đến file Bun app của bạn
+    script: 'server.js',          // Đường dẫn đến file Bun app của bạn
     interpreter: 'bun',        // Sử dụng Bun làm runtime
-    args: 'run dev1',               // Thêm args nếu cần thiết
+    args: 'run',               // Thêm args nếu cần thiết
     instances: 1,              // Tùy chọn: số lượng instance
     exec_mode: 'fork',         // Chạy theo kiểu fork hoặc cluster
     port: 1905,                // Đảm bảo ứng dụng nghe trên cổng 1905
+    env: {
+      NODE_ENV: 'production',  // Môi trường chạy là production
+    },
   },
 ]
 }
